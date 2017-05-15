adf374a4556110c28dd50b088b6daa00b54ce967
Richard Garcia <richardgarciaph@gmail.com>
2017-05-15 13:23:55 +1200

    add first components and css bootstrap

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,19 @@
 // console.log("it works!!");
-
 import React from "react";
 import {render} from "react-dom"
+
+class App extends React.Component {
+  render() {
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>Hello World React!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById("app"));
